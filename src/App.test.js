bc96297916fca42957be882c8904715fd55a3c65
eyobtadele2705 <89188494+eyobtadele2705@users.noreply.github.com>
2025-05-09 06:39:45 +0000

    fix

--- src/App.test.js
+++ src/App.test.js
@@ -4,5 +4,6 @@ import App from './App';
 test('renders learn Jenkins link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn Jenkins/i);
+  console.log('This is test.');
   expect(linkElement).toBeInTheDocument();
 });
